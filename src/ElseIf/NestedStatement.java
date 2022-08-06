f7d91de4d2afa46ea1ffb25f672d4618e1cb2b72
Saiprasad <saiprasadpatil1010@gmail.com>
2022-08-06 09:45:51 +0530

    first commit

---
+++ src/ElseIf/NestedStatement.java
@@ -0,0 +1,19 @@
+package ElseIf;
+
+public class NestedStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=10,b=15;
+		if (a==10) {
+		if (b!=15)	{
+			System.out.println("Saiprasad");
+		} else {
+			System.out.println("Bytesquare");
+		}
+		}
+		
+	}
+
+}
